244ac8a558227673c0021c0b33378c62c300b00f
Marcin Rak <56159101+marcrak403@users.noreply.github.com>
2023-07-28 16:10:52 +0200

    Update CurrencyHistory.cs

--- InvestApp/InvestApp.Core/Models/CurrencyHistory.cs
+++ InvestApp/InvestApp.Core/Models/CurrencyHistory.cs
@@ -5,6 +5,7 @@ namespace InvestApp.Core.Models
     public class CurrencyHistory
     {
         public string Table { get; set; } = string.Empty;
+        public string New { get; set; } = string.Empty;
         public string Currency { get; set; } = string.Empty;
         public string Code { get; set; } = string.Empty;
         public IEnumerable<CurrencyRates> Rates { get; set; }
