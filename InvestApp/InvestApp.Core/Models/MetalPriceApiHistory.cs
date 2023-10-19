806ad0a218961969ef301db4a644f2477e997fcb
Marcin Rak <56159101+marcrak403@users.noreply.github.com>
2023-10-19 15:15:02 +0200

    Update MetalPriceApiHistory.cs

--- InvestApp/InvestApp.Core/Models/MetalPriceApiHistory.cs
+++ InvestApp/InvestApp.Core/Models/MetalPriceApiHistory.cs
@@ -6,7 +6,11 @@
         public string Base { get; set; } = string.Empty;
         public string start_date {  get; set; }
         public string end_date { get; set;}
-        public IDictionary<string, IDictionary<string , double>> Rates { get; set; }
-
+        public RatesData Rates { get; set; }
+        
+        public class RatesData
+        {
+            public IDictionary<string, IDictionary<string, double>> Data { get; } = new Dictionary<string, IDictionary<string, double>>();
+        }
     }
 }
