23522ac18d0538343b8d3a8b96decf082dc8964a
Administrator <>
2024-02-27 15:34:59 +0100

    fork [ DEV2 ]

--- InvestApp/InvestApp.DataAccess/AuthenticationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InvestApp
-{
-    public class AuthenticationSettings
-    {
-        public string? JwtKey { get; set; }
-        public int DurationInMinutes { get; set; }
-        public string? JwtIssuer { get; set; }
-    }
-}
